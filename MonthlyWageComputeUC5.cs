f47418e681b7b2988286892d513398834b440d6f
Jp <jyotiprakashpc@outlook.com>
2022-07-30 22:10:28 +0530

    Monthly Wage Compute

---
+++ MonthlyWageComputeUC5.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationProgram
+{
+    internal class MonthlyWageComputeUC5
+    {
+        static int wage_Per_Hour = 20;
+        static int full_Time_Hour = 8;
+        static int part_Time_Hour = 4;
+        static int work_Hour = 0;
+        static int total_Wage;
+
+        public static void getAttendance()
+        {
+            // 0 for Absent
+            // 1 for Present
+            // 2 for PartTime
+            Random random = new Random();
+            for (int i = 0; i < 20; i++)
+            {
+                int attendance = random.Next(3);
+                if (attendance == 0)
+                {
+                    Console.WriteLine("Employee is Absent");
+                    work_Hour += 0;
+                }
+                else if (attendance == 2)
+                {
+                    Console.WriteLine("Part Time Employee");
+                    work_Hour += part_Time_Hour;
+                }
+                else
+                {
+                    Console.WriteLine("Employee is Present");
+                    work_Hour += full_Time_Hour;
+                }
+            }
+            calculateWage();
+        }
+        public static void calculateWage()
+        {
+            total_Wage = work_Hour * wage_Per_Hour;
+            Console.WriteLine("Total Work Hour is " + work_Hour);
+            Console.WriteLine("Monthly Wage is " + total_Wage);
+        }
+    }
+}
