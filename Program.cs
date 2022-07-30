14cb1fd68989440c3c52a5eec26db36d28c1f23d
Jp <jyotiprakashpc@outlook.com>
2022-07-30 21:55:10 +0530

    Welome Message

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace EmployeeWageComputationProgram
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            //Call to get attendence
+            DailyWageUC2.getAttendance();
+        }
+    }
+}
