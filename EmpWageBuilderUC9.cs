20e45a5f38accec80dbf18e3889ac7af4eb37e6f
Jp <jyotiprakashpc@outlook.com>
2022-08-08 22:30:43 +0530

    Ability to save total wage

---
+++ EmpWageBuilderUC9.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationProgram
+{
+    public class EmpWageBuilderUC9
+    {
+        char choice;
+        public void CompanyBuilder(int no_Of_Company)
+        {
+            int num = no_Of_Company;
+            WageCalculatorUC9[] wageCalculator = new WageCalculatorUC9[num];
+            float[] total_Wage = new float[num];
+
+            for (int i = 0; i < num; i++)
+            {
+                wageCalculator[i] = new WageCalculatorUC9();
+                Console.WriteLine("Enter Values for Wage Calculation of Company "+ (i+1) +".");
+                Console.Write("Enter Employe Wage Per Hour = ");
+                float wage = float.Parse(Console.ReadLine());
+                wageCalculator[i].wage_Per_Hour = wage;
+                Console.Write("Enter No. Of Working Days Per Month = ");
+                int month = Convert.ToInt32(Console.ReadLine());
+                wageCalculator[i].no_Of_Days_Per_Month = month;
+                Console.Write("Enter Total Working Hour Per Month = ");
+                int total_Hour = Convert.ToInt32(Console.ReadLine());
+                wageCalculator[i].work_Hour_Per_Month = total_Hour;
+                wageCalculator[i].getAttendance();
+                total_Wage[i] = wageCalculator[i].calculateWage();                 //Saving total wage for each company
+            }
+            for (int i = 0; i < num; i++)
+                Console.WriteLine("Total Wage for Company "+ (i+1) +" =" + total_Wage[i]);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,8 +5,16 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wage Computation Program");
-            //Call to get attendence
-            DailyWageUC2.getAttendance();
+
+            Console.Write("Enter No. Of Company = ");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            EmpWageBuilderUC9[] empWageBuilderUC9 = new EmpWageBuilderUC9[num];
+            for (int i = 0; i < num; i++)
+            {
+                empWageBuilderUC9[i] = new EmpWageBuilderUC9();
+                empWageBuilderUC9[i].CompanyBuilder(num);
+            }
         }
     }
 }
